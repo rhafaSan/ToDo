09c0a5c29f9f855925b6c9f89fb8bf7dad704b89
Rhafael Santos <rhafa99ss@gmail.com>
2020-07-20 18:08:14 -0300

    Toggle function done

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,61 +1,86 @@
-import React, {Component} from 'react';
-import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, Platform } from 'react-native';
 
 import todayImage from '../../assets/imgs/today.jpg';
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import commonStyles from '../commonStyles';
 
-
 import Task from '../components/Task'
 
 export default class TaskList extends Component {
+
+  state = {
+    showDoneTasks: true,
+    visibleTasks: [],
+    tasks: [{
+      id: Math.random(),
+      desc: 'Comprar Livro',
+      estimateAt: new Date(),
+      doneAt: new Date()
+    },{
+      id: Math.random(),
+      desc: 'Ler Livro',
+      estimateAt: new Date(),
+      doneAt: null
+    }]
+  }
+
+  componentDidMount = () => {
+    this.filterTasks()
+  }
+
+  toggleFilter = () => {
+    this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks)
+  }
+
+  filterTasks = () => {
+    let visibleTasks = null
+    if(this.state.showDoneTasks){
+      visibleTasks = [...this.state.tasks]
+    }else{
+      const pending = task => task.doneAt === null
+      visibleTasks = this.state.tasks.filter(pending) 
+    }
+
+    this.setState({ visibleTasks })
+  }
+
+  toggleTask = taskId => {
+    const tasks = [...this.state.tasks]
+    tasks.forEach(task => {
+      if(task.id === taskId){
+        task.doneAt = task.doneAt ? null : new Date()
+      }
+    })
+
+    this.setState({ tasks },this.filterTasks)
+  }
+
   render() {
     const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
     return (
       <View style={styles.container}>
         <ImageBackground source={todayImage} style={styles.background}>
-          <View style={styles.titleBar}>
-            <Text style={styles.title}>Hoje</Text>
-            <Text style={styles.subTitle}>{today}</Text>
+          <View style={styles.iconBar}>
+            <TouchableOpacity onPress={this.toggleFilter}>
+              <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'} size={20} 
+              color={commonStyles.colors.secondary} />
+            </TouchableOpacity>
           </View>
+          <View style={styles.titleBar}> 
+            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.subTitle}>{today}</Text>  
+          </View>          
         </ImageBackground>
-       <View style={styles.taskList}>
+        <View style={styles.taskList}>
 
-        <Task desc="Comprar Livro" estimateAt={new Date()}
-         doneAt={new Date()}/>
-        <Task desc="Ler Livro" estimateAt={new Date()}
-         doneAt={null}/>
-         <Task desc="Comprar Livro" estimateAt={new Date()}
-         doneAt={new Date()}/>
-        <Task desc="Ler Livro" estimateAt={new Date()}
-         doneAt={null}/>
-         <Task desc="Comprar Livro" estimateAt={new Date()}
-         doneAt={new Date()}/>
-        <Task desc="Ler Livro" estimateAt={new Date()}
-         doneAt={null}/>
-         <Task desc="Comprar Livro" estimateAt={new Date()}
-         doneAt={new Date()}/>
-        <Task desc="Ler Livro" estimateAt={new Date()}
-         doneAt={null}/>
-         <Task desc="Comprar Livro" estimateAt={new Date()}
-         doneAt={new Date()}/>
-        <Task desc="Ler Livro" estimateAt={new Date()}
-         doneAt={null}/>
-         <Task desc="Comprar Livro" estimateAt={new Date()}
-         doneAt={new Date()}/>
-        <Task desc="Ler Livro" estimateAt={new Date()}
-         doneAt={null}/>
-         <Task desc="Comprar Livro" estimateAt={new Date()}
-         doneAt={new Date()}/>
-        <Task desc="Ler Livro" estimateAt={new Date()}
-         doneAt={null}/>
-         <Task desc="Comprar Livro" estimateAt={new Date()}
-         doneAt={new Date()}/>
-        <Task desc="Ler Livro" estimateAt={new Date()}
-         doneAt={null}/>
-       </View>
+         <FlatList data={this.state.visibleTasks} keyExtractor={item => `${item.id}`} 
+         renderItem={({item}) =><Task {...item} toggleTask={this.toggleTask} />}/>
+          
+        </View>
       </View>
     );
   }
@@ -68,14 +93,14 @@ const styles = StyleSheet.create({
   background: {
     flex: 3
   },
-  taskList:{
+  taskList: {
     flex: 7
   },
-  titleBar:{
+  titleBar: {
     flex: 1,
     justifyContent: 'flex-end'
   },
-  title:{
+  title: {
     fontFamily: commonStyles.fontFamily,
     color: commonStyles.colors.secondary,
     fontSize: 50,
@@ -88,6 +113,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginLeft: 20,
     marginBottom: 30
+  }, 
+  iconBar:{
+    flexDirection: 'row',
+    marginHorizontal: 20,
+    justifyContent: 'flex-end',
+    marginTop: Platform.OS === 'ios' ? 30 : 10
   }
 
 });
